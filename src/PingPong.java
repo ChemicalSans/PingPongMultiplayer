3e1fb49301aa0232d55982709fcd002bbca200b3
Lekoopa55 <lekoopa55@gmail.com>
2021-06-18 08:49:53 +0200

    Sans's commiting massage :Y

--- src/PingPong.java
+++ src/PingPong.java
@@ -1,7 +1,22 @@
 public class PingPong implements Runnable{
+    Thread thread = new Thread(this);
+
+    public PingPong () {
+
+        thread.start();
+    }
 
     @Override
     public void run() {
+        while (true) {
+            try {
+                thread.sleep(20);
 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
