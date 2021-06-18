136c9fb7df4e5d30220c73234d953d3789c14b12
Alexander Nolz <alexander.nolz>
2021-06-18 09:29:03 +0200

    ImageLoader

---
+++ src/Client/ImageLoader.java
@@ -0,0 +1,15 @@
+package Client;
+
+public class ImageLoader {
+
+
+
+    public ImageLoader() {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
