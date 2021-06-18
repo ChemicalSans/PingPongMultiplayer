7cbba46b1942370eebc48843284c114e35251d91
Alexander Nolz <alexander.nolz>
2021-06-18 11:49:07 +0200

    ImageLoader

--- src/Client/ImageLoader.java
+++ src/Client/ImageLoader.java
@@ -1,13 +1,17 @@
 package Client;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class ImageLoader {
 
-    BufferedImage image;
+    BufferedImage öe;
 
     public ImageLoader() {
         try {
+
+            öe = ImageIO.read(new File("src\\Sprites\\Skin_Österreich.png"));
 
         } catch (Exception e) {
             e.printStackTrace();
