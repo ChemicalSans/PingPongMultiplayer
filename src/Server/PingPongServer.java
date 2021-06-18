24097ab24aa4685e60c69e5fb82c86442d3d9e70
Alexander Nolz <alexander.nolz>
2021-06-18 13:56:17 +0200

    ImageLoader

--- src/Server/PingPongServer.java
+++ src/Server/PingPongServer.java
@@ -1,10 +1,16 @@
 package Server;
 
 import java.io.IOException;
+import java.util.Vector;
 
 public class PingPongServer implements Runnable {
     int portPlayerOne = 11111;
     int portPlayerTwo = 11112;
+    int vx=3;//Geschwindigkeit X
+    int vy=3;//Geschwindigkeit Y
+    int bx;//Ball position X
+    int by;//Ball position Y
+    boolean col;//collision
 
 
     PlayerConnection playerOneConnection = null;
@@ -27,6 +33,17 @@ public class PingPongServer implements Runnable {
         while (true) {
             if(playerOneConnection.socket.isConnected()) {
                 System.out.println("[INFO] Player one connected!");
+            }
+
+            //MOVE
+
+
+
+
+
+            //überprüft ob der ball mit einemobjekt kolidiert ist was vorher überprüft wurde
+            if(col == true){
+                vx*=-1;vy*=-1;
             }
         }
     }
